d779e4773a75602cc6564e8c6e4d6696e1ce24df
ahmed2402 <ahmedraza312682@gmail.com>
2025-03-17 20:14:04 +0500

    done

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import random
 randomNum = random.randint(1,100)
 
-while True:
-    guess = input("Guess the Number or Quit(Q) : ")
-    if(guess == "Q"):
-        break
+def check_guess(): 
+ while True:
+        guess = input("Guess the Number or Quit(Q) : ")
+        if(guess == "Q"):
+            break
 
-    guess = int(guess)
+        guess = int(guess)
+    
+        if(guess == randomNum):
+            print(f"Congrats! {guess} is the correct answer!\n")
+            break
+        elif(guess<randomNum):
+            print(f"{guess} is too small! Take a bigger guess")
+        else:
+            print(f"{guess} is too big! Take a smaller guess")
 
-    if(guess == randomNum):
-        print(f"Congrats! {guess} is the correct answer!\n")
-        break
-    elif(guess<randomNum):
-        print(f"{guess} is too small! Take a bigger guess")
-    else:
-        print(f"{guess} is too big! Take a larger guess")
+check_guess()            
 
 print("___GAME____OVER__")        
+
+
